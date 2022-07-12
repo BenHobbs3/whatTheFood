97142f8d2f1616dd89e85284696d5fcba0764e17
Ben Hobbs <benhobbs2022@gmail.com>
2022-07-12 09:31:26 -0500

    added requirements

---
+++ js/magic.js
@@ -0,0 +1,10 @@
+$(function () {
+    $('.slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        prevArrow: '<span class = "prev_arrow"> <img src = "../images/arrowLeft.png"/></span>',
+        nextArrow: '<span class = "right_arrow"> <img src="../images/arrowRight.png" alt="lasagna image" />',
+      });
+});
+
