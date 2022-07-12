b30d94b2159630644662a024c6c44546098251f5
Ben Hobbs <benhobbs@Bens-Mac-mini.lan>
2022-07-12 12:00:40 -0500

    Fixed arrows

--- js/magic.js
+++ js/magic.js
@@ -1,10 +1,12 @@
 $(function () {
     $('.slider').slick({
+        centerMode: true,
+        centerPadding: '60px',
         infinite: true,
         slidesToShow: 3,
         slidesToScroll: 1,
         prevArrow: '<span class = "prev_arrow"> <img src = "../images/arrowLeft.png"/></span>',
-        nextArrow: '<span class = "right_arrow"> <img src="../images/arrowRight.png" alt="lasagna image" />',
+        nextArrow: '<span class = "next_arrow"> <img src="../images/arrowRight.png"/></span>',
       });
 });
 
