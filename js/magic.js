47b0b99c8c7df48c05142fe1cbd2023adddad079
Ben Hobbs <benhobbs2022@gmail.com>
2022-07-15 15:16:26 -0500

    final

--- js/magic.js
+++ js/magic.js
@@ -1,12 +1,37 @@
 $(function () {
     $('.slider').slick({
+        // centerMode: true,
+        // centerPadding: '60px',
+        // infinite: true,
+        // slidesToShow: 3,
+        // slidesToScroll: 1,
+
         centerMode: true,
         centerPadding: '60px',
-        infinite: true,
         slidesToShow: 3,
-        slidesToScroll: 1,
+        responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 3
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 1
+            }
+          }
+        ],
         prevArrow: '<span class = "prev_arrow"> <img src = "../images/arrowLeft.png"/></span>',
         nextArrow: '<span class = "next_arrow"> <img src="../images/arrowRight.png"/></span>',
       });
 });
+
 
